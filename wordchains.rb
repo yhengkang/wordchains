3c9960a23185db96133df10cc6c8680ed5fa97fe
yhengkang <yhengkang@gmail.com>
2013-09-22 21:42:25 -0700

    finished first version

--- wordchains.rb
+++ wordchains.rb
@@ -68,105 +68,113 @@ end
 
 class WordChains
 	DICTIONARY = File.readlines("dictionary.txt").map(&:chomp)
-	attr_reader :same_length_words, :start_word
+	attr_reader :same_length_words, :start_word, :visited_words
 
-	def initialize
-		@start_words = ["beer"]
+	def initialize(start_word)
+		@start_word = TreeNode.new(start_word)
+		@visited_nodes = [@start_word]
+		# @visited_words = [@start_word.value]
 		@same_length_words = []
-		@possible_words = []
-		@visited_words = []  ##Everytime new words is found, it is added to @visited words
-		@current_words = []
-		@new_words = []
-		@end_word = ""
-	end
-
-	def new_to_start
-		puts "Adding new_words to start_words"
-		@start_words += new_words
-		puts "Clearing new_words"
-		@new_words = []
-	end
-
-	def set_start(start_word)
-		@start_words << start_word
-	end
-
-	def set_end(end_word)
-		@end_word = end_word
-	end
-
-	def found_word
-		puts "Found word!"
-		p @new_words
 	end
 
 	def same_length 
-		@same_length_words = DICTIONARY.select{|word| word.length == @start_words.first.length }
-		@same_length_words -= @visited_words
+		@same_length_words = DICTIONARY.select{|word| word.length == @start_word.value.length }
 	end
 
-	def one_letter_diff?(word1, word2)
-		different_letters = 0
-		word1.split('').each_with_index do |letter, i|
-			different_letters += 1 unless letter == word2[i]
+	def one_letter_diff?(word1,word2)
+		letter_difference = 0
+		word1.length.times do |i|
+			letter_difference += 1 unless word1[i] == word2[i]
 		end
-		if different_letters == 1
-			true
-		else
-			false
+		return true if letter_difference == 1
+		return false
+	end
+
+	def build_word_tree(end_word)
+		# puts "does this work?"
+		visited_words = [@start_word.value]
+		until visited_words.include?(end_word)
+			print "."
+			@visited_nodes.each do |node|
+				@same_length_words.each do |same_length_word|
+					if one_letter_diff?(node.value, same_length_word) && !visited_words.include?(same_length_word)
+						visited_words << same_length_word
+						@visited_nodes << node.add_child(same_length_word)
+						@same_length_words -= [same_length_word]
+					end
+				end
+			end
 		end
+	end
+
+	def find_word_chain(end_word)
+		@visited_nodes.first.bfs(end_word).find_path
+	end
+
+	def run_command(end_word)
+		self.same_length
+		build_word_tree(end_word)
+		find_word_chain(end_word)
 	end
 
 #take each word in start_words
 #creates an similiar_words array of all words that are similar
-#takes from similar_words array words not in new_words
-	def create_new_words(unvisited_words)
-		@start_words.each do |word|
-			similar_words = unvisited_words.select {|unvisited_word| one_letter_diff?(word, unvisited_word)}
-			@new_words += similar_words.select {|similar_word| !@new_words.include?(similar_word)}
-			return found_word if @new_words.include?(@end_word)
-		end
-		@visited_words += @new_words
-		@new_words
-	end
+# #takes from similar_words array words not in new_words
+# 	def create_new_words(unvisited_words)
+# 		@start_words.each do |word|
+# 			similar_words = unvisited_words.select {|unvisited_word| one_letter_diff?(word, unvisited_word)}
+# 			@new_words += similar_words.select {|similar_word| !@new_words.include?(similar_word)}
+# 			return found_word if @new_words.include?(@end_word)
+# 		end
+# 		@visited_words += @new_words
+# 		@new_words
+# 	end
 			
-	def test_run
-		same_length #populates same_length_words
-		create_new_words(@same_length_words)
-	end
-
-
+# 	def test_run
+# 		same_length #populates same_length_words
+# 		create_new_words(@same_length_words)
+# 	end
 
 end
 
-
-	# def one_letter_diff
-	# 	@same_length_words.each do |word|
-	# 		different_letters = @start_word.split('') - word.split('')
-	# 		if different_letters.length == 1
-	# 			@possible_words << word
-	# 		end
-	# 	end
-	# 	@possible_words
-	# end
-	#Doesnt work because the array doesnt care which letter comes first.  So abbe - beer = 1, since bs and es cancel
-
-	# def one_letter_diff
-	# 	@same_length_words.each do |word|
-	# 		different_letters = 0
-	# 		word.split('').each_with_index do |letter,i|
-	# 			unless letter == @start_word[i]
-	# 				different_letters += 1
+	# def build_word_tree(end_word)
+	# 	self.same_length
+	# 	until @visited_words.include?(end_word)
+	# 		@visited_nodes.each do |node|
+	# 			valid_words = []
+	# 			one_letter_diff(node.value, @same_length_words).each do |possible_word|
+	# 				valid_words << possible_word unless @visited_words.include?(possible_word)
+	# 				@visited_words += valid_words 	
+	# 				valid_words.each do |valid_word|
+	# 					node.add_child(valid_word)
+	# 				end
 	# 			end
-	# 		end
-	# 		@possible_words << word if different_letters == 1
+	# 		end	
 	# 	end
-	# 	@possible_words
-	# end   		#test run done, now gotta modulize
+	# 	@visited_nodes.last
+	# end
 
 
+	# def one_letter_diff?(word1, word2)
+	# 	different_letters = 0
+	# 	word1.split('').each_with_index do |letter, i|
+	# 		different_letters += 1 unless letter == word2[i]
+	# 	end
+	# 	if different_letters == 1
+	# 		true
+	# 	else
+	# 		false
+	# 	end
+	# end
 
-	# def create_possible_words
-	# 	same_length
-	# 	one_letter_diff
+	# def one_letter_diff(word1, word_array)
+	# 	possible_words = []
+	# 	word_array.each do |word2|
+	# 		letter_difference = 0
+	# 		word1.length.times do |i|
+	# 			letter_difference += 1 if word1[i] != word2[i]
+	# 		end
+	# 		possible_words << word2 if letter_difference == 1
+	# 	end
+	# 	possible_words
 	# end
